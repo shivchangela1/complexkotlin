266e518d995f7dacad7772b82d215f9f24b4a310
shivchangela1 <shivc@uw.edu>
2019-04-15 21:35:19 -0700

    Finished assignment

--- main.kts
+++ main.kts
@@ -5,7 +5,20 @@ println("UW Complex Kotlin homework")
 // use fold to compress the array of strings down into a single string
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ
 //
-val mapFoldResults = ""
+var mapFoldResults = ""
+var map = HashMap<Int, String>()
+for (i in 1..15) {
+    when { 
+        i % 3 == 0 && i % 5 == 0 -> map.put(i, "FIZZBUZZ")
+        i % 3 == 0 -> map.put(i, "FIZZ")
+        i % 5 == 0 -> map.put(i, "BUZZ")
+        else -> map.put(i, "")
+    }
+}
+for ((key, value) in map) {
+    mapFoldResults += "$value"
+}
+
 
 
 // This is a utility function for your use as you choose, and as an
@@ -20,22 +33,48 @@ fun Int.times(block: () -> Unit): Unit {
 fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
-val r1 = "" // call process() with message "FOO" and a block that returns "BAR"
-
+val r1 = process("FOO", block = {"BAR"})// call process() with message "FOO" and a block that returns "BAR"
 val r2_message = "wooga"
-val r2 = "" // call process() with message "FOO" and a block that upper-cases 
+val r2 = process("FOO", block = {r2_message.toUpperCase().repeat(3)}) // call process() with message "FOO" and a block that upper-cases 
             // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
 
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+enum class Philosopher() { 
+    THINKING {
+        override fun signal() = TALKING
+        override fun toString(): String {
+            return "Deep thoughts...."
+        }
+    },
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString(): String {
+            return "Allow me to suggest an idea..."
+        }
+    };
+
+    abstract fun signal(): Philosopher
+
+}
 
 // create an class "Command" that can be used as a function (provide an "invoke()" function)
 // that takes a single parameter ("message" of type String)
 // primary constructor should take a String argument ("prompt")
 // when called, the Command object should return a String containing the prompt and then the message
 class Command(val prompt: String) {
+    operator fun invoke(message: String): String { 
+        return prompt + message
+    } 
 }
+
+/*EXTRA CREDIT 
+1. Seneca (/ˈsɛnɪkə/), was a Roman Stoic philosopher, statesman, dramatist, and—in one work—satirist 
+of the Silver Age of Latin literature.
+2. The school of philosophy he is most associate with is Stoicism. 
+3. Stoicism asserts that virtue is happiness and judgment should be based on 
+behavior, rather than words. 
+*/
 
 
 
